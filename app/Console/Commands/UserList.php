3622bed12ffe2dcc6ccdf869f075c68ef05f23b3
Vildan Sarıoğlu <vildansarioglu38@gmail.com>
2018-07-02 15:31:34 +0300

    First commit

---
+++ app/Console/Commands/UserList.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+
+class UserList extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:message';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'User List';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $headers = ['Name', 'Email'];
+
+        $users = User::all(['name', 'email'])->toArray();
+
+        $this->table($headers, $users);
+    }
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app');
+
+@section('content')
+    <h3>Right Aligned Navbar</h3>
+    <p>The .navbar-right class is used to right-align navigation bar buttons.</p>
+
+@endsection
